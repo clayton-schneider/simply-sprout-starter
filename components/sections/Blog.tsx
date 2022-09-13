5d34fe396ed0a12d94aa3fb2355d293386ba42dd
Clayton Schneider <schneiderc2016@gmail.com>
2022-09-13 14:09:25 -0400

    initial pagebuilder setup

---
+++ components/sections/Blog.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import { BlogSection } from "../../typings";
+
+const Blog: FunctionComponent<BlogSection> = () => {
+  return <h2>Blog Section</h2>;
+};
+
+export default Blog;
